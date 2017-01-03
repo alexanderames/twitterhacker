6f25f2a67f886d94c9fa181883cb026179bfce90
alexander ames <alexanderthomasames@gmail.com>
2017-01-03 12:22:09 -0700

    add user, tweet validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  validates :username, presence: true, uniqueness: true
+
   has_many :tweets
+
+  before_create :prepend_at_symbol
+
+  def prepend_at_symbol
+  	self.username.prepend("@")
+  end
 end
