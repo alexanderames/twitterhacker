d44e6fdbee31222908b41de4a11ee3c58b08f3be
alexander ames <alexanderthomasames@gmail.com>
2017-01-03 13:54:17 -0700

    add follow functionality to controller and corresponding views

--- app/controllers/epicenter_controller.rb
+++ app/controllers/epicenter_controller.rb
@@ -1,11 +1,30 @@
 class EpicenterController < ApplicationController
   def feed
+  	#initialize array that holds tweets from current_user follow list
+  	@following_tweets = []
+
+  	#iterate thru and find associated user tweets in array
+  	Tweet.all.each do |tweet|
+  		if current_user.following.include?(tweet.user_id)
+  			@following_tweets.push(tweet)
+  			#tweets pushed into array to power view
+  		end
+  	end
   end
 
   def show_user
+  	@user = User.find(params[:id])
   end
 
   def now_following
+  	#displaying purposes
+  	@user = User.find(params[:follow_id])
+
+  	current_user.following.push(params[:follow_id].to_i)
+  	#adds user.id of User you want to follow to 'following' array attribute
+
+  	current_user.save
+  	#save to db
   end
 
   def unfollow
